a07416535d0041e01d55fcbb0aed9b809f39c46f
Ngonidzashe Nzenze <ngonidzashedelight@gmail.com>
2025-06-10 06:17:46 +0200

    added filtering

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q
 
 from .models import Company, Job, Resume, Application
 from . import serializers
-from .permissions import ReadOnly, IsOwnerOrReadOnly
+from .permissions import ReadOnly, IsOwnerOrReadOnly, IsAdminOrOwner
 
 
 class CompanyViewset(viewsets.ModelViewSet):
@@ -16,6 +18,8 @@ class JobViewset(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = serializers.JobSerializer
     permission_classes = [IsAdminUser | ReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["title", "location", "job_type"]
 
 
 class ResumeViewset(viewsets.ModelViewSet):
@@ -23,8 +27,21 @@ class ResumeViewset(viewsets.ModelViewSet):
     serializer_class = serializers.ResumeSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class ApplicationViewset(viewsets.ModelViewSet):
     queryset = Application.objects.all()
     serializer_class = serializers.ApplicationSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminOrOwner]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["status"]
+
+    def get_queryset(self):
+        return self.queryset.filter(
+            Q(user=self.request.user) | Q(job__posted_by__is_admin=True)
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
