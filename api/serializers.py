a4c983e3a2ad5e5e8717058c1d9200d31b7a1a68
Ngonidzashe Nzenze <ngonidzashedelight@gmail.com>
2025-06-16 05:50:21 +0200

    converted to hyperlinked serializers

--- api/serializers.py
+++ api/serializers.py
@@ -3,25 +3,31 @@ from rest_framework import serializers
 from .models import Company, Job, Resume, Application
 
 
-class CompanySerializer(serializers.ModelSerializer):
+class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Company
         fields = "__all__"
+        extra_kwargs = {"url": {"view_name": "company-detail", "lookup_field": "pk"}}
 
 
-class JobSerializer(serializers.ModelSerializer):
+class JobSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Job
         fields = "__all__"
+        extra_kwargs = {"url": {"view_name": "job-detail", "lookup_field": "pk"}}
 
 
-class ResumeSerializer(serializers.ModelSerializer):
+class ResumeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Resume
         fields = "__all__"
+        extra_kwargs = {"url": {"view_name": "resume-detail", "lookup_field": "pk"}}
 
 
-class ApplicationSerializer(serializers.ModelSerializer):
+class ApplicationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Application
         fields = "__all__"
+        extra_kwargs = {
+            "url": {"view_name": "application-detail", "lookup_field": "pk"}
+        }
