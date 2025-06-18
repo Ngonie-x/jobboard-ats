673770d3114d3b3d3b14f863b88d1e5b4a4a7430
Ngonidzashe Nzenze <ngonidzashedelight@gmail.com>
2025-06-18 05:25:06 +0200

    minor db update

--- api/models.py
+++ api/models.py
@@ -11,6 +11,9 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Companies"
+
 
 class Job(models.Model):
     JOB_TYPE_CHOICES = [
