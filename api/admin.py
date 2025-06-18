c491a598fc8f6605b49efb3a524adb7666a3a314
Ngonidzashe Nzenze <ngonidzashedelight@gmail.com>
2025-06-18 05:24:26 +0200

    added admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company, Job, Resume, Application
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "website"]
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ["title", "company", "location", "job_type"]
+    list_filter = ["company", "job_type"]
+
+
+@admin.register(Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ["user", "uploaded_at"]
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ["job", "user", "status", "applied_at"]
