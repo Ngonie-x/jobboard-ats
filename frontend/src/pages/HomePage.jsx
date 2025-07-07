ab6b41bb1a07204d4780e76607269c9455a8e3de
Ngonidzashe Nzenze <ngonidzashedelight@gmail.com>
2025-07-07 06:00:54 +0200

    simplified homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,20 +1,7 @@
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
 import Home from '../components/Home';
 
 const HomePage = () => {
-    const { logout } = useContext(AuthContext);
-
-    return (
-        <>
-            <div>
-                <h1>Job List</h1>
-                <button onClick={logout}>Logout</button>
-            </div>
-            <Home/>
-        </>
-        
-    );
+    return <Home/>
 };
 
 export default HomePage;
